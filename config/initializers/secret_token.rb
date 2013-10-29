f70b62639de53e96a5b4d10d6831a245d21e5743
mlevinPUT <mlevin@mike-ubuntu.(none)>
2013-10-29 07:59:54 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeSlips::Application.config.secret_token = 'c6e8d94864b7aeba7589fb13c12826351acaab2ea85aa56b2e5fda7e2138080ac1b0a282ba7a0cb83f861c3812d0ccdf7357c7280ddb8945817194c1489e6255'
